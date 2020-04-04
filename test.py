3c2852860f79839f5ef6412f1662874d5fb1dda6
thomasvn <thomasnguyen96@gmail.com>
2020-04-03 20:13:05 -0700

    Playing around with a directed graph and colored edges

--- test.py
+++ test.py
@@ -5,22 +5,24 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-DATA_FILE = 'data/test.json'
+DATA_FILE = 'data/47b3bb0e99cef4dbf43d6ce37cbf87f700334d77.json'
+DEFAULT_NODE_SIZE = 300  # library default is 300
 
 # Retrieve the json data on dependencies
 with open(DATA_FILE) as f:
     data = json.load(f)
 
 # Generate edge list
-top_url = data['top_url']
-edgelist = [ (top_url, ext_url) for ext_url in data['external_urls'] ]
-nodesize = [ data['external_urls'][ext_url]['count']*300 for ext_url in data['external_urls'] ]
-nodesize.insert(0, 300)  # top_url
-print(nodesize)
+edgelist = []
+node_sizes = [DEFAULT_NODE_SIZE]
+top_domain = data['top_domain']
+for ext_domain in data['external_domains']:
+    edgelist.append((top_domain, ext_domain))
+    node_sizes.append(data['external_domains'][ext_domain]['resources']['total'] * DEFAULT_NODE_SIZE)
 
 # Generate Graph
 G = nx.from_edgelist(edgelist)
 
 # Plot it
-nx.draw(G, with_labels=True, node_color="skyblue", node_size=nodesize)
+nx.draw(G, with_labels=True, font_size=5, node_color="skyblue", node_size=node_sizes)
 plt.show()
