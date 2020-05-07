8c6d545146a469c213d947ec0da6784fab7bb9a7
thomasvn <thomasnguyen96@gmail.com>
2020-05-07 13:23:22 -0700

    Attempting to identify shared dependencies. Stuck in adjusting node sizes

--- dep_graph_shared.py
+++ dep_graph_shared.py
@@ -1,15 +1,11 @@
-''' Plot the dependency graph for a multiple top level domains (TLD) '''
+''' Plot the shared dependencies across multiple top level domains (TLD) '''
 import os
 import json
-import numpy as np
-import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-DEFAULT_NODE_SIZE = 1  # library default is 300
-MIN_DEPENDENCIES = 5
+DEFAULT_NODE_SIZE = 150
 DATA_DIR = 'data/pgh/'
-# RESOURCES = ['script', 'iframe', 'video', 'audio', 'img', 'embed']
 RESOURCES = ['script']
 
 
@@ -31,14 +27,22 @@ for tld in TLDs:
     for ext_domain in ext_domains:
         edgelist.append((top_domain, ext_domain))
 
+# Generate list of shared dependences
+# A dependency is shared when "external_domain" exists for many "top_domains"
+shared_deps_edgelist = []
+for edge in edgelist:
+    all_other_ext_domains = [e[1] for e in edgelist if e != edge]
+    if edge[1] in all_other_ext_domains:
+        shared_deps_edgelist.append(edge)
+
 # Generate Graph
-G = nx.from_edgelist(edgelist)
+G = nx.from_edgelist(shared_deps_edgelist)
 
 
 ################################################################################
 # Parameter adjusting (labels, node sizes, etc.)
 ################################################################################
-# Count the total number of dependent resources for each TLD
+# Count the number of dependencies (used to determine node size)
 num_deps = {}
 for tld in TLDs:
     # Add the top domain into the dictionary
@@ -54,23 +58,14 @@ for tld in TLDs:
         num_deps[ext] = num_deps[ext] + total if ext in num_deps else total
 
 # Update node sizes in the graph
-node_sizes = []
-for node in G.nodes:
-    node_sizes.append(num_deps[node] * DEFAULT_NODE_SIZE)
-
-# Update node labels in the graph
-labels = {}
-for node, size in zip(G.nodes, node_sizes):
-    if size >= MIN_DEPENDENCIES:
-        labels[node] = node
-    elif node in [tld['top_domain'] for tld in TLDs]:
-        labels[node] = node
-    else:
-        labels[node] = ''
+node_sizes = [num_deps[node] * DEFAULT_NODE_SIZE for node in G.nodes]
+print(node_sizes)
+print(len(G.nodes))
+print(len(node_sizes))
 
 
 ################################################################################
 # Drawing
 ################################################################################
-nx.draw(G, with_labels=True, font_color='r', font_size=5, node_color="skyblue", node_size=node_sizes, labels=labels)
+nx.draw(G, with_labels=True, font_color='k', font_size=5, node_sizes=node_sizes)
 plt.show()
