d4ddb57c9215a050a4dc326619741a19a9bbf454
thomasvn <thomasnguyen96@gmail.com>
2020-05-07 09:37:31 -0700

    Update graphing to only look at specific resources

--- dep_graph_multi.py
+++ dep_graph_multi.py
@@ -6,37 +6,42 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-DATA_DIR = 'data_pgh2/'
 DEFAULT_NODE_SIZE = 1  # library default is 300
+DATA_DIR = 'data/pgh/'
+RESOURCES = ['script', 'iframe', 'video', 'audio', 'img', 'embed']
 
 # Retrieve the json data on dependencies
-json_data = []
+TLDs = []
 for fname in os.listdir(DATA_DIR):
     if not fname.endswith('.json'): continue
     with open(DATA_DIR + fname) as f:
-        json_data.append(json.load(f))
+        TLDs.append(json.load(f))
 
 # Generate list of edges between all nodes
 edgelist = []
-for data in json_data:
-    top_domain = data['top_domain']
-    ext_domain = data['external_domains']
-    for ed in ext_domain:
-        edgelist.append((top_domain, ed))
+for tld in TLDs:
+    top_domain = tld['top_domain']
+    ext_domains = tld['external_domains']
+    for ext_domain in ext_domains:
+        edgelist.append((top_domain, ext_domain))
 
 # Generate Graph
 G = nx.from_edgelist(edgelist)
 
-# Count the total number of dependencies
+# Count the total number of dependent resources for each TLD
 num_deps = {}
-for data in json_data:
-    for ed in data['external_domains']:
-        total = data['external_domains'][ed]['resources']['total']
-        if ed in num_deps: num_deps[ed] = num_deps[ed] + total
-        else: num_deps[ed] = total
-    td = data['top_domain']
-    if td in num_deps: num_deps[td] = num_deps[td] + 1
-    else: num_deps[td] = 1
+for tld in TLDs:
+    # Add the top domain into the dictionary
+    td = tld['top_domain']
+    num_deps[td] = num_deps[td] + 1 if td in num_deps else 1
+
+    # Add the external domains into the dictionary
+    ext_domains = tld['external_domains']
+    for ext in ext_domains:
+        total = 0
+        for r in RESOURCES:
+            total += ext_domains[ext]['resources'][r]
+        num_deps[ext] = num_deps[ext] + total if ext in num_deps else total
 
 # Update node sizes in the graph
 node_sizes = []
